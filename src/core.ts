1052eb244c6f45ad38eb8c2dd0163062c34a79d5
AndyTitu <andi.titu@agilebits.com>
2024-01-24 16:18:20 +0100

    Move test core to another file

--- src/core.ts
+++ src/core.ts
@@ -49,32 +49,3 @@ export class SharedCore implements Core {
 
   async releaseClient(clientId: number): Promise<void> {}
 }
-
-// TestCore is a mocked Core used only for testing.
-export class TestCore implements Core {
-  id: number;
-
-  constructor(id: number) {
-    this.id = id;
-  }
-  async initClient(config: ClientAuthConfig): Promise<string> {
-    const res = this.id.toString();
-    this.id++;
-    return res;
-  }
-
-  async invoke(config: InvokeConfig): Promise<string> {
-    return (
-      "method " +
-      config.invocation.name +
-      " called on client " +
-      config.clientId +
-      " with parameters " +
-      config.invocation.parameters
-    );
-  }
-
-  async releaseClient(clientId: number) {
-    this.id--;
-  }
-}
