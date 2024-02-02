edeaba13497d825dd531c738390f5fa5d3b8a32f
AndyTitu <andi.titu@agilebits.com>
2024-02-02 14:42:42 +0100

    Clean up core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,8 +1,4 @@
-import {
-  init_client,
-  invoke,
-  release_client,
-} from "./js-core/core.js";
+import { init_client, invoke, release_client } from "./js-core/core.js";
 
 // `Core` exposes the WASM core interface to the host JS SDK.
 export interface Core {
@@ -46,21 +42,13 @@ interface Invocation {
 // SharedCore is an implementation of the Core interface that shares resources across all clients.
 export class SharedCore implements Core {
   async initClient(config: ClientAuthConfig): Promise<string> {
-    const c = JSON.stringify(config);
-    console.log("before init_client in JS SDK")
-    const id = await init_client(c);
-    console.log("after init_client in JS SDK")
-    console.log(id)
-    return id;
+    const serializedConfig = JSON.stringify(config);
+    return await init_client(serializedConfig);
   }
 
   async invoke(config: InvokeConfig): Promise<string> {
-    const c = JSON.stringify(config);
-    console.log("before invoke in JS SDK")
-    const response = await invoke(c);
-    console.log("after invoke in JS SDK")
-    console.log(response)
-    return response;
+    const serializedConfig = JSON.stringify(config);
+    return await invoke(serializedConfig);
   }
 
   releaseClient(clientId: number): void {
