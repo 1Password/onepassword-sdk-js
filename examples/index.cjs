2407ee38b338534fd0a32002350b69f9d0757472
AndyTitu <andi.titu@agilebits.com>
2024-02-14 17:28:59 +0100

    Format code

--- examples/index.cjs
+++ examples/index.cjs
@@ -6,7 +6,7 @@ async function fetchSecret() {
     auth: process.env.OP_SERVICE_ACCOUNT_TOKEN,
     integrationName: sdk.DEFAULT_INTEGRATION_NAME,
     integrationVersion: sdk.DEFAULT_INTEGRATION_VERSION,
-  })
+  });
 
   // Fetches a secret.
   return await client.secrets.resolve("op://vault/item/field");
