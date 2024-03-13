9a8053653d4e5319a25236c65f6a617065e3657f
Sam Nolan <sam.nolan@agilebits.com>
2024-03-13 09:47:13 -0400

    update require path for test file

--- client/test/task.js
+++ client/test/task.js
@@ -1,4 +1,4 @@
-const { createClient, DEFAULT_INTEGRATION_VERSION } = require("../dist/client.js");
+const { createClient, DEFAULT_INTEGRATION_VERSION } = require("../dist/client_util.js");
 const { parentPort, isMainThread, workerData } = require('node:worker_threads');
 
 async function retrieveSecret() {
