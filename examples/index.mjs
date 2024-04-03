8eeddc82562f8451d505c0747acb729ca0a28675
Sam Nolan <sam.nolan@agilebits.com>
2024-04-03 10:44:34 -0400

    change example reference

--- examples/index.mjs
+++ examples/index.mjs
@@ -8,5 +8,6 @@ const client = await createClient({
 });
 
 // Fetches a secret.
-const secret = await client.secrets.resolve("op://vault/item/field");
+// See syntax here: https://developer.1password.com/docs/cli/secrets-reference-syntax/
+const secret = await client.secrets.resolve("op://SDKs Test/TestLogin/username");
 console.log(secret)
