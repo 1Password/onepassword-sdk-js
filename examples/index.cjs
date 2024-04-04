874a54882a42e1490c20cd8c2a09d76d0c989dfc
Sam Nolan <sam.nolan@agilebits.com>
2024-04-04 14:00:11 -0400

    correct secret reference in correct file

--- examples/index.cjs
+++ examples/index.cjs
@@ -9,7 +9,7 @@ async function fetchSecret() {
   });
 
   // Fetches a secret.
-  return await client.secrets.resolve("op://gowwbvgow7kxocrfmfvtwni6vi/6ydrn7ne6mwnqc2prsbqx4i4aq/password");
+  return await client.secrets.resolve("op://SDKs Test/Test Login/test_username");
 }
 
 fetchSecret().then(console.log)
