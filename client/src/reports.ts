992c8bf158f4de1609729f2120a606733045d7aa
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-04-06 21:13:57 +0000

    Update core to version 43d085d2

---
+++ client/src/reports.ts
@@ -0,0 +1,41 @@
+import { InvokeConfig, InnerClient } from "./core.js";
+import * as types from "./types.js";
+
+export interface ReportsApi {
+  getAccountOverview(): Promise<types.AccountOverview>;
+  getDevicesOverview(): Promise<types.DevicesOverview>;
+}
+
+export class ReportsSource implements ReportsApi {
+  #inner: InnerClient;
+
+  public constructor(inner: InnerClient) {
+    this.#inner = inner;
+  }
+
+  public async getAccountOverview(): Promise<types.AccountOverview> {
+    const invocationConfig: InvokeConfig = {
+      clientId: this.#inner.id,
+      invocation: {
+        name: "GetAccountOverview",
+        parameters: {},
+      },
+    };
+    return JSON.parse(
+      await this.#inner.core.invoke(invocationConfig),
+    ) as Promise<types.AccountOverview>;
+  }
+
+  public async getDevicesOverview(): Promise<types.DevicesOverview> {
+    const invocationConfig: InvokeConfig = {
+      clientId: this.#inner.id,
+      invocation: {
+        name: "GetDevicesOverview",
+        parameters: {},
+      },
+    };
+    return JSON.parse(
+      await this.#inner.core.invoke(invocationConfig),
+    ) as Promise<types.DevicesOverview>;
+  }
+}
