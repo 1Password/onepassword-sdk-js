9f5472ab84cce2c3e8ad932eacee99b1821a5218
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-04-24 15:30:53 +0000

    Update core to version 1cf804ad

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,11 +1,13 @@
 import { InnerClient } from "./core.js";
 import { SecretsApi, SecretsSource } from "./secrets.js";
+import { ItemsApi, ItemsSource } from "./items.js";
 
-// Client represents a client instance of the SDK.
 export class Client {
   public secrets: SecretsApi;
+  public items: ItemsApi;
 
   public constructor(innerClient: InnerClient) {
     this.secrets = new SecretsSource(innerClient);
+    this.items = new ItemsSource(innerClient);
   }
 }
