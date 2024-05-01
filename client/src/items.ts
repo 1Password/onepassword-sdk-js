f7ae67f2a85568733f33e2555dda2a6fc1dd74e0
Horia Culea <horia.culea@agilebits.com>
2024-05-01 16:53:57 +0200

    Add item create

--- client/src/items.ts
+++ client/src/items.ts
@@ -1,8 +1,10 @@
 import { InvokeConfig, InnerClient } from "./core.js";
-import * as types from "./types.js";
+import * as types from "./types.js"
 
 export interface ItemsApi {
-  get(vaultId: string, itemId: string): Promise<types.Item>;
+	create(item: types.Item): Promise<types.Item>;
+	get(vaultId: string, itemId: string): Promise<types.Item>;
+	
 }
 
 export class ItemsSource implements ItemsApi {
@@ -12,19 +14,43 @@ export class ItemsSource implements ItemsApi {
     this.#inner = inner;
   }
 
-  public async get(vaultId: string, itemId: string): Promise<types.Item> {
-    const invocationConfig: InvokeConfig = {
-      clientId: this.#inner.id,
-      invocation: {
-        name: "Get",
-        parameters: {
-          vault_id: vaultId,
-          item_id: itemId,
-        },
-      },
-    };
-    return JSON.parse(
-      await this.#inner.core.invoke(invocationConfig),
-    ) as Promise<types.Item>;
-  }
+public async create(item: types.Item): Promise<types.Item> {
+
+
+	const invocationConfig: InvokeConfig = {
+		clientId: this.#inner.id,
+		invocation: {
+		name: "Create",
+		parameters: {
+			item: item,
+			
+			},
+		},
+	};
+	return JSON.parse(await this.#inner.core.invoke(invocationConfig)) as Promise<types.Item>;
+}
+
+
+
+public async get(vaultId: string, itemId: string): Promise<types.Item> {
+
+
+	const invocationConfig: InvokeConfig = {
+		clientId: this.#inner.id,
+		invocation: {
+		name: "Get",
+		parameters: {
+			vault_id: vaultId,
+			item_id: itemId,
+			
+			},
+		},
+	};
+	return JSON.parse(await this.#inner.core.invoke(invocationConfig)) as Promise<types.Item>;
+}
+
+
+
+
+
 }
