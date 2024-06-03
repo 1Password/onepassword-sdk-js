50ec47dffb3706468304658fe28c3fcf2c50abbe
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-06-03 15:46:06 +0000

    Update core to version e88645c7

--- client/src/secrets.ts
+++ client/src/secrets.ts
@@ -1,7 +1,16 @@
 import { InvokeConfig, InnerClient } from "./core.js";
 import * as types from "./types.js";
 
+/**
+ *  Secrets point to fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
+ */
+/**
+ *  Read more about secrets: https://developer.1password.com/docs/cli/secret-references
+ */
 export interface SecretsApi {
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   resolve(secretReference: string): Promise<string>;
 }
 
@@ -12,6 +21,9 @@ export class SecretsSource implements SecretsApi {
     this.#inner = inner;
   }
 
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   public async resolve(secretReference: string): Promise<string> {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
