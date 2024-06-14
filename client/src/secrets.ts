40f79a17d2cf85114cfc9f589670290f06ee5b18
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-06-14 18:37:27 +0000

    Update core to version d2f073c4

--- client/src/secrets.ts
+++ client/src/secrets.ts
@@ -1,7 +1,14 @@
 import { InvokeConfig, InnerClient } from "./core.js";
 import * as types from "./types.js";
 
+/**
+ *  The Secrets API includes all operations the SDK client can perform on secrets.
+ *  Use secret reference URIs to securely load secrets from 1Password: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
+ */
 export interface SecretsApi {
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   resolve(secretReference: string): Promise<string>;
 }
 
@@ -12,6 +19,9 @@ export class SecretsSource implements SecretsApi {
     this.#inner = inner;
   }
 
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   public async resolve(secretReference: string): Promise<string> {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
