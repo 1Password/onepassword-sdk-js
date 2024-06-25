b6832805343b7da9271b18e94fb662461d3daa3f
Omar Miraj <omar.miraj@agilebits.com>
2024-06-25 12:49:43 -0400

    Add newlines

--- client/release/templates/version.tpl.js
+++ client/release/templates/version.tpl.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SDK_BUILD_NUMBER = exports.SDK_VERSION = void 0;
 exports.SDK_VERSION = "{{ version }}";
 exports.SDK_BUILD_NUMBER = "{{ build }}";
+
--- client/release/version.js
+++ client/release/version.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SDK_BUILD_NUMBER = exports.SDK_VERSION = void 0;
 exports.SDK_VERSION = "0.1.0-beta.11";
 exports.SDK_BUILD_NUMBER = "0010011";
+
