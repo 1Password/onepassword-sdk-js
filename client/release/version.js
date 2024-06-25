c8ea8be53132c6abc01a5ab11e71f5531b7740e4
Omar Miraj <omar.miraj@agilebits.com>
2024-06-25 11:48:12 -0400

    fix version

--- client/release/version.js
+++ client/release/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SDK_BUILD_NUMBER = exports.SDK_VERSION = void 0;
-exports.SDK_VERSION = "0.1.0-beta.13";
-exports.SDK_BUILD_NUMBER = "0010013";
+exports.SDK_VERSION = "0.1.0-beta.12";
+exports.SDK_BUILD_NUMBER = "0010012";
