6579d72fc34eeebbe1b0b84743fe17595b1db561
Horia Culea <45151601+hculea@users.noreply.github.com>
2024-07-26 14:23:46 +0200

    Update configuration.ts

--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -1,6 +1,7 @@
-import { ClientAuthConfig } from "./core.js";
 import { SDK_BUILD_NUMBER } from "../release/version.js";
 import os from "os";
+
+import { ClientAuthConfig } from "./core.js";
 
 export const LANGUAGE = "JS";
 export const VERSION = SDK_BUILD_NUMBER;
