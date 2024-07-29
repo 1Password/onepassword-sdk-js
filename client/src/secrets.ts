07fb50e75ecd927ab2530cf4f4c6ca9b70a80d46
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-07-29 14:47:22 +0000

    Update core to version bd02a7a7

--- client/src/secrets.ts
+++ client/src/secrets.ts
@@ -1,7 +1,15 @@
 import { InvokeConfig, InnerClient } from "./core.js";
 import * as types from "./types.js";
+import { OpList } from "./iterator.js";
 
+/**
+ *  The Secrets API includes all operations the SDK client can perform on secrets.
+ *  Use secret reference URIs to securely load secrets from 1Password: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
+ */
 export interface SecretsApi {
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   resolve(secretReference: string): Promise<string>;
 }
 
@@ -12,11 +20,14 @@ export class SecretsSource implements SecretsApi {
     this.#inner = inner;
   }
 
+  /**
+   *  Resolve returns the secret the provided secret reference points to.
+   */
   public async resolve(secretReference: string): Promise<string> {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
       invocation: {
-        name: "Resolve",
+        name: "SecretsResolve",
         parameters: {
           secret_reference: secretReference,
         },
---
+++ client/src/vaults.ts
@@ -0,0 +1,37 @@
+import { InvokeConfig, InnerClient } from "./core.js";
+import * as types from "./types.js";
+import { OpList } from "./iterator.js";
+
+/**
+ *  The Vaults API holds all the operations the SDK client can perform on 1Password vaults.
+ */
+export interface VaultsApi {
+  /**
+   *  List all vaults
+   */
+  list(): Promise<OpList<types.VaultOverview>>;
+}
+
+export class VaultsSource implements VaultsApi {
+  #inner: InnerClient;
+
+  public constructor(inner: InnerClient) {
+    this.#inner = inner;
+  }
+
+  /**
+   *  List all vaults
+   */
+  public async list(): Promise<OpList<types.VaultOverview>> {
+    const invocationConfig: InvokeConfig = {
+      clientId: this.#inner.id,
+      invocation: {
+        name: "VaultsList",
+        parameters: {},
+      },
+    };
+    return new OpList<types.VaultOverview>(
+      JSON.parse(await this.#inner.core.invoke(invocationConfig)),
+    );
+  }
+}
