cdaa448325c7c79f4470da3cdc42d8ccf6a84236
AndyTitu <andi.titu@agilebits.com>
2024-08-01 12:29:20 +0200

    Update version to latest

--- client/release/version.js
+++ client/release/version.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SDK_BUILD_NUMBER = exports.SDK_VERSION = void 0;
-exports.SDK_VERSION = "0.1.0-beta.12";
-exports.SDK_BUILD_NUMBER = "0010012";
+exports.SDK_VERSION = "0.1.0-beta.14";
+exports.SDK_BUILD_NUMBER = "0010014";
 const SDK_CORE_VERSION = "0.1.0-beta.12";
 
