05a5e014ccc6d8126ed8dc35b4b78c901e84b32a
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-08-01 13:32:23 +0000

    Update core to version c24e2b60

--- client/src/items.ts
+++ client/src/items.ts
@@ -1,5 +1,6 @@
 import { InvokeConfig, InnerClient } from "./core.js";
 import * as types from "./types.js";
+import { SdkIterable } from "./iterator.js";
 
 /**
  *  The Items API holds all operations the SDK client can perform on 1Password items.
@@ -24,6 +25,11 @@ export interface ItemsApi {
    *  Delete an item.
    */
   delete(vaultId: string, itemId: string): Promise<void>;
+
+  /**
+   *  List all items
+   */
+  list(vaultId: string): Promise<SdkIterable<types.ItemOverview>>;
 }
 
 export class ItemsSource implements ItemsApi {
@@ -40,7 +46,7 @@ export class ItemsSource implements ItemsApi {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
       invocation: {
-        name: "Create",
+        name: "ItemsCreate",
         parameters: {
           params,
         },
@@ -58,7 +64,7 @@ export class ItemsSource implements ItemsApi {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
       invocation: {
-        name: "Get",
+        name: "ItemsGet",
         parameters: {
           vault_id: vaultId,
           item_id: itemId,
@@ -77,7 +83,7 @@ export class ItemsSource implements ItemsApi {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
       invocation: {
-        name: "Put",
+        name: "ItemsPut",
         parameters: {
           item,
         },
@@ -95,7 +101,7 @@ export class ItemsSource implements ItemsApi {
     const invocationConfig: InvokeConfig = {
       clientId: this.#inner.id,
       invocation: {
-        name: "Delete",
+        name: "ItemsDelete",
         parameters: {
           vault_id: vaultId,
           item_id: itemId,
@@ -103,5 +109,25 @@ export class ItemsSource implements ItemsApi {
       },
     };
     await this.#inner.core.invoke(invocationConfig);
+  }
+
+  /**
+   *  List all items
+   */
+  public async list(vaultId: string): Promise<SdkIterable<types.ItemOverview>> {
+    const invocationConfig: InvokeConfig = {
+      clientId: this.#inner.id,
+      invocation: {
+        name: "ItemsList",
+        parameters: {
+          vault_id: vaultId,
+        },
+      },
+    };
+    return new SdkIterable<types.ItemOverview>(
+      JSON.parse(
+        await this.#inner.core.invoke(invocationConfig),
+      ) as types.ItemOverview[],
+    );
   }
 }
