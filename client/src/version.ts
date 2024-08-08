5d43d0a1f173407d7f493d772cc17fa5e34d4521
AndyTitu <andi.titu@agilebits.com>
2024-08-08 15:11:37 +0300

    Update versions used in code as well

--- client/src/version.ts
+++ client/src/version.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION = "0.1.0";
-export const SDK_BUILD_NUMBER = "0010017";
+export const SDK_VERSION = "0.1.1";
+export const SDK_BUILD_NUMBER = "0010100";
 const SDK_CORE_VERSION = "0.1.0-beta.15";
 
