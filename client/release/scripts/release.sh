5e91d48836c3ec6543b588fdb23084aef03d66de
Omar Miraj <omar.miraj@agilebits.com>
2024-10-17 09:02:39 -0400

    Update release script with some nits

--- client/release/scripts/release.sh
+++ client/release/scripts/release.sh
@@ -25,19 +25,17 @@ cleanup() {
 # waits for NPM to publish on internal registry to update package json
 wait_for_npm_publish(){
     package_name="${1}"
+    updated_npm_version="${2}"
 
-    # Assign the correct version based off if its core or not
-    updated_npm_version=$( [[ "$package_name" == *"core"* ]] && echo "$version_sdk_core" || echo "$version_sdk" )
-    
     while true; do
-    npm_verison=$(npm view "$package_name" version)
+    npm_version=$(npm view "$package_name" version)
     # if package matches npm registry than break out of loop and update package.json
-    if [ "$npm_verison" == "$updated_npm_version" ]; then
-        echo "The package version $npm_verison is up to date!"
+    if [ "$npm_version" == "$updated_npm_version" ]; then
+        echo "The package version $npm_version is up to date!"
         break
     else
         # Sleep for another 5 seconds if it still didnt sync
-        echo "The package version $npm_verison is not up to date. Latest version is $updated_npm_version."
+        echo "The $updated_npm_version package version is not yet published to NPM. Waiting..."
         sleep 5
     fi
 done
@@ -89,7 +87,7 @@ fi
   cd client
   if [ "$core_modified" = true ]; then
     # Wait for npm to be update to the latest version
-    wait_for_npm_publish @1password/sdk-core
+    wait_for_npm_publish @1password/sdk-core "${version_sdk_core}"
     # Update @1password/sdk-core dependancy to the latest
     npm install @1password/sdk-core@latest -E
   fi
@@ -110,7 +108,7 @@ fi
             npm dist-tag add @1password/sdk@${version_sdk} latest
 
             # Sleep for about 12s to allow npm registry to publish the latest version
-            wait_for_npm_publish @1password/sdk
+            wait_for_npm_publish @1password/sdk "${version_sdk}"
             # Update dependency in examples to run off the latest SDK
             cd ../examples && npm install @1password/sdk@latest -E
 
