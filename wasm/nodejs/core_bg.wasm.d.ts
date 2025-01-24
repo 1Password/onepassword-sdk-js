6abd785425dc45e409a24cf29a3f978ae088d77e
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2025-01-24 22:00:17 +0000

    Update core to version 0f204adc

--- wasm/nodejs/core_bg.wasm.d.ts
+++ wasm/nodejs/core_bg.wasm.d.ts
@@ -1,15 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function init_client(a: number, b: number): number;
-export function invoke(a: number, b: number): number;
-export function invoke_sync(a: number, b: number, c: number): void;
-export function release_client(a: number, b: number, c: number): void;
-export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const init_client: (a: number, b: number) => any;
+export const invoke: (a: number, b: number) => any;
+export const invoke_sync: (a: number, b: number) => [number, number, number, number];
+export const release_client: (a: number, b: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd4c20bed3310a910(a: number, b: number, c: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number, c: number): void;
-export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__h7697bc37b01801c3(a: number, b: number, c: number, d: number): void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const closure1817_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1899_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
