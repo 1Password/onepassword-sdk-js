c89fe9394c638f3c7269b36b6fa63135127e8b1a
Horia Culea <horia.culea@agilebits.com>
2025-03-03 14:28:38 +0100

    Use consts instead of funcs

--- client/src/types.ts
+++ client/src/types.ts
@@ -398,7 +398,7 @@ export enum WordListType {
   ThreeLetters = "threeLetters",
 }
 
-export function ReviverFunc(key: string, value: unknown): unknown {
+export const ReviverFunc = (key: string, value: unknown): unknown => {
   if (
     Array.isArray(value) &&
     value.every((v) => Number.isInteger(v) && v >= 0 && v <= 255) &&
@@ -409,7 +409,7 @@ export function ReviverFunc(key: string, value: unknown): unknown {
   return value;
 }
 
-export function ReplacerFunc(key: string, value: unknown): unknown {
+export const ReplacerFunc = (key: string, value: unknown): unknown => {
   if (value instanceof Uint8Array) {
     return Array.from(value);
   }
