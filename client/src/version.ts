3b0a058252ea746081674a9b1c8d9b2fc98fa91b
Omar Miraj <omar.miraj@agilebits.com>
2025-03-20 07:03:25 -0400

    Release v0.2.1

--- client/src/version.ts
+++ client/src/version.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION = "0.2.0";
-export const SDK_BUILD_NUMBER = "0020001";
-const SDK_CORE_VERSION = "0.2.0";
+export const SDK_VERSION = "0.2.1";
+export const SDK_BUILD_NUMBER = "0020002";
+const SDK_CORE_VERSION = "0.2.1";
 
