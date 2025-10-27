f08a2cf27954c3c0731b110039c024bbd1eb4a21
Eddy Filip <eddy.filip@agilebits.com>
2025-10-27 21:19:11 +0100

    Add example codes for beta features

--- examples/index-desktop.cjs
+++ examples/index-desktop.cjs
@@ -3,6 +3,12 @@ const sdk = require("@1password/sdk");
 // [developer-docs.sdk.js/common-js.sdk-import]-end
 
 async function main() {
+  const vaultId = process.env.OP_VAULT_ID;
+
+  if (!vaultId) {
+    throw new Error("Missing required environment variable: OP_VAULT_ID")
+  }
+
   // [developer-docs.sdk.js.client-initialization]-start
   // Connects to the 1Password desktop app.  
   const client = await sdk.createClient({
@@ -20,18 +26,101 @@ async function main() {
   }
   // [developer-docs.sdk.python.list-vaults]-end
 
-  const vaultId = process.env.OP_VAULT_ID;
-
-  if (!vaultId) {
-    throw new Error("Missing required environment variable: OP_VAULT_ID");
-  }
-
   // [developer-docs.sdk.python.list-items]-start
   const items = await client.items.list(vaultId);
   for await (const item of items) {
     console.log(item.id + " " + item.title);
   }
   // [developer-docs.sdk.python.list-items]-end
+
+  // Vault get overview
+  const vaultOverview = await client.vaults.getOverview(vaultId);
+  console.log(JSON.stringify(vaultOverview));
+
+  // Vault get
+  const vault = await client.vaults.get(vaultOverview.id, {accessors: false});
+  console.log(JSON.stringify(vault));
+
+  itemsToCreate = [];
+  for (let i = 1; i <= 3; i++) {
+    itemsToCreate.push({
+      title: `My Login Item ${i}`,
+      category: sdk.ItemCategory.Login,
+      vaultId: vault.id,
+      fields: [
+        {
+          id: "username",
+          title: "username",
+          fieldType: sdk.ItemFieldType.Text,
+          value: "my username",
+        },
+        {
+          id: "password",
+          title: "password",
+          fieldType: sdk.ItemFieldType.Concealed,
+          value: "my secret value",
+        },
+        {
+          id: "onetimepassword",
+          title: "one-time password",
+          sectionId: "custom section",
+          fieldType: sdk.ItemFieldType.Totp,
+          value:
+            "otpauth://totp/my-example-otp?secret=jncrjgbdjnrncbjsr&issuer=1Password",
+        },
+      ],
+      sections: [
+        {
+          id: "custom section",
+          title: "my section",
+        },
+      ],
+      tags: ["test tag 1", "test tag 2"],
+      websites: [
+        {
+          url: "example.com",
+          label: "url",
+          autofillBehavior: sdk.AutofillBehavior.AnywhereOnWebsite,
+        },
+      ],
+    })
+  }
+
+  // Batch item create
+  const batchCreateResponse = await client.items.createAll(vault.id, itemsToCreate)
+
+  let itemIDs = [];
+  for (const res of batchCreateResponse.individualResponses) {
+    if (res.content) {
+      console.log(`Created item "${res.content.title}" (${res.content.id})`);
+      itemIDs.push(res.content.id);
+    }
+    else if (res.error) {
+      console.log(`[Batch create] Something went wrong: ${res.error}`);
+    }
+  }
+
+  // Batch item get
+  const batchGetResponse = await client.items.getAll(vault.id, itemIDs);
+  for (const res of batchGetResponse.individualResponses) {
+    if (res.content) {
+      console.log(`Obtained item "${res.content.title}" (${res.content.id})`);
+    }
+    else if (res.error) {
+      console.log(`[Batch get] Something went wrong: ${res.error}`);
+    }
+  }
+
+  // Batch item delete
+  const batchDeleteResponse = await client.items.deleteAll(vault.id, itemIDs);
+  for (const [id, res] of Object.entries(batchDeleteResponse.individualResponses)) {
+    if (res.error) {
+      console.log(`[Batch delete] Something went wrong: ${res.error}`);
+    }
+    else {
+      console.log(`Deleted item ${id}`);
+    }
+  }
 }
 
 main();
