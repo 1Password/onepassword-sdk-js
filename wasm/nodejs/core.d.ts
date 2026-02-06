6f2cf67b239d026db4fee5d990cfb2a1733a24f0
AndyTitu <andi.titu@agilebits.com>
2026-02-06 17:02:47 +0100

    Use the wasm which only has stable features

--- wasm/nodejs/core.d.ts
+++ wasm/nodejs/core.d.ts
@@ -1,18 +1,46 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
+ * Initializes an SDK client with a given configuration.
+ */
+export function init_client(config: string): Promise<string>;
+/**
  * Handles all asynchronous invocations to the SDK core received from the SDK.
  */
 export function invoke(parameters: string): Promise<string>;
 /**
- * Initializes an SDK client with a given configuration.
+ * Handles all synchronous invocations to the SDK core received from the SDK.
  */
-export function init_client(config: string): Promise<string>;
+export function invoke_sync(parameters: string): string;
 /**
  * Drops a client, releasing the memory allocated for it.
  */
 export function release_client(client_id: string): void;
 /**
- * Handles all synchronous invocations to the SDK core received from the SDK.
+ * The `ReadableStreamType` enum.
+ *
+ * *This API requires the following crate features to be activated: `ReadableStreamType`*
  */
-export function invoke_sync(parameters: string): string;
+type ReadableStreamType = "bytes";
+export class IntoUnderlyingByteSource {
+  private constructor();
+  free(): void;
+  start(controller: ReadableByteStreamController): void;
+  pull(controller: ReadableByteStreamController): Promise<any>;
+  cancel(): void;
+  readonly type: ReadableStreamType;
+  readonly autoAllocateChunkSize: number;
+}
+export class IntoUnderlyingSink {
+  private constructor();
+  free(): void;
+  write(chunk: any): Promise<any>;
+  close(): Promise<any>;
+  abort(reason: any): Promise<any>;
+}
+export class IntoUnderlyingSource {
+  private constructor();
+  free(): void;
+  pull(controller: ReadableStreamDefaultController): Promise<any>;
+  cancel(): void;
+}
