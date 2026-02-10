6a5fc959723c1006af6e56d05732c01174d68d99
AndyTitu <andi.titu@agilebits.com>
2026-02-10 12:02:20 +0100

    Release v0.4.1

--- client/src/version.ts
+++ client/src/version.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION = "0.4.0-beta.2";
-export const SDK_BUILD_NUMBER = "0040002";
-const SDK_CORE_VERSION = "0.4.0-beta.2";
+export const SDK_VERSION = "0.4.1";
+export const SDK_BUILD_NUMBER = "0040003";
+const SDK_CORE_VERSION = "0.4.1";
 
